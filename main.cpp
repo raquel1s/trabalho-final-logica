1ceb1e3567ba0def1e6a3b11e0b876b630fd2f9e
margkarp-pro <lucas07.fernandes@catolicasc.edu.br>
2026-06-09 19:11:54 -0300

    new function menu_print

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 
 int main() {
-	
     return 0;
+}
+
+void menu_print(void){
+    system("cls");
+    printf("=========MENU=========\n\n");
+    printf("[1] INCLUIR CONTATO\n");
+    printf("[2] LISTA DE CONTATOS\n");
+    printf("[3] CONSULTAR CONTATO PELO NOME\n");
+    printf("[4] EXCLUIR CONTATO\n");
+    printf("[5] SAIR\n");
 }
