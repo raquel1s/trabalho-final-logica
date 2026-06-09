412e9fa889683b5f7f5f70321a8d65553c3f8cea
raquel1s <silvaraquel.0729@gmail.com>
2026-06-09 19:15:03 -0300

    removendo o opa.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 100
+
+void menu_print(void);
+Contato incluir_contato(void);
+void listar_contatos(void);
+void consultar_contato_nome(void);
+void excluir_contato(void);
+
+typedef struct {
+	char nome[50];
+	char telefone[20];
+} Contato;
+
+Contato lista_contatos[MAX];
 
 int main() {
 	
