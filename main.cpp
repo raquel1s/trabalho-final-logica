1deb88f86e0972501be8f236b04cbae9ec0d9468
raquel1s <silvaraquel.0729@gmail.com>
2026-06-09 19:28:00 -0300

    função de incluir usuario

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 #define MAX 100
 
@@ -11,42 +10,15 @@ typedef struct {
 } Contato;
 
 void menu_print(void);
-Contato incluir_contato(void);
+void incluir_contato(void);
 void listar_contatos(void);
 void consultar_contato_nome(void);
 void excluir_contato(void);
 
 Contato lista_contatos[MAX];
+int qnt_contatos = 0;
 
 int main() {
-    int choice;
-    do
-    {
-        menu_print();
-        switch (choice)
-        {
-        case 1:
-
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-        case 4:
-        
-            break;
-        default:
-            system("cls");
-            for(int i = 0;i < 5;i++){
-                printf("ERRO -scolha invalida");
-                sleep(1000);
-            }
-            break;
-        }
-    } while (choice != 5);
-    
     return 0;
 }
 
@@ -58,4 +30,22 @@ void menu_print(void){
     printf("[3] CONSULTAR CONTATO PELO NOME\n");
     printf("[4] EXCLUIR CONTATO\n");
     printf("[5] SAIR\n");
+}
+
+void incluir_contato(void){
+	system(cls);
+	printf("=========INCLUIR USUARIO=========\n\n");
+	
+	if(qnt_contatos < MAX){
+		printf("Digite o nome do contato: ");
+		fgets(lista_contatos[qnt_contatos].nome, 50, stdin);
+		
+		printf("Digite o telefone do contato: ");
+		fgets(lista_contatos[qnt_contatos].telefone, 20, stdin);
+		
+		qnt_contatos++;
+		printf("\nContato salvo com sucesso!");
+	}else{
+		printf("\nAgenda lotada!");
+	}	
 }
